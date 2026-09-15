b613e56d3ce9eec0e0a4b97ebaba979fc612ec2b
123456 <123456@123456>
2026-09-15 23:29:49 +0800

    bendi commit 222

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,6 +5,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-
+        System.out.printf("bendi commit 222");
     }
 }
